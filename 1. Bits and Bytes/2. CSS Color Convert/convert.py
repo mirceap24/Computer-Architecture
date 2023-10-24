3de8d8ce04a0598a08f530ab48e0b08a8ca309b0
mirceap24 <24mirceap@gmail.com>
2023-10-24 13:18:31 +0300

    simple.css + advanced.css done

--- 1. Bits and Bytes/2. CSS Color Convert/convert.py
+++ 1. Bits and Bytes/2. CSS Color Convert/convert.py
@@ -14,9 +14,21 @@ def xx_to_dec(xx):
     return (d[xx[0]] << 4) + d[xx[1]] 
 
 def hex_to_rgb(r):
-    hx =  r.group(1)
+    hx =  r.group(1).lower()
+    # normalize to non-abbreviated form
+    if len(hx) in {3, 4}:
+        hx = ''.join(x + x for x in hx)
+    # compute decimal form of R, G and B
     dec = [xx_to_dec(hx[i: i + 2]) for i in (0, 2, 4)]
-    return f'rgb({" ".join(str(d) for d in dec)})'
+    # maybe compute alpha channel 
+    if len(hx) == 6:
+        label = 'rgb'
+    elif len(hx) == 8:
+        label = 'rgba'
+        dec.append(xx_to_dec(hx[6:]) / 255)
+    else:
+        return f'#{hx}' # TODO not the best way to validate
+    return f'{label}({" ".join(str(d) for d in dec)})'
 
 sys.stdout.write(re.sub(r'\#([0-9a-fA-F]+)', hex_to_rgb, sys.stdin.read()))
 
